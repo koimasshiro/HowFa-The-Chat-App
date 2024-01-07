cfa0ce645ab910c7a1d4440426172e5b1bba7901
koimasshiro <ese.imhariagbe@gmail.com>
2024-01-07 21:33:51 +0100

    Added search user functionality #2

--- client/src/Components/UserListItem/UserListItem.jsx
+++ client/src/Components/UserListItem/UserListItem.jsx
@@ -1,9 +1,41 @@
+import { Avatar, Box, Text } from '@chakra-ui/react';
 import React from 'react';
 
 
 const UserListItem = ({user, handleFunc}) => {
   return (
-    <div>UserListItem</div>
+    <div> <Box
+    onClick={handleFunc}
+    cursor="pointer"
+    bg="#E8E8E8"
+    _hover={{
+      background: "#38B2AC",
+      color: "white",
+    }}
+    w="100%"
+    d="flex"
+    alignItems="center"
+    color="black"
+    px={3}
+    py={2}
+    mb={2}
+    borderRadius="lg"
+  >
+    <Avatar
+      mr={2}
+      size="sm"
+      cursor="pointer"
+      name={user.name}
+      src={user.pic}
+    />
+    <Box>
+      <Text>{user.name}</Text>
+      <Text fontSize="xs">
+        <b>Email : </b>
+        {user.email}
+      </Text>
+    </Box>
+  </Box></div>
   )
 }
 
